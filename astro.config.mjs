a9ef78743418ff67c2a6e0dcaa4b8b9f27431908
tasyadew <github@tasyadew.com>
2024-05-29 19:05:19 +0800

    Initial Commit

---
+++ astro.config.mjs
@@ -0,0 +1,9 @@
+import { defineConfig } from 'astro/config';
+import compress from "astro-compress";
+
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [compress(), tailwind()]
+});
